5eda17ca080df3c3513069618afb24b146d63eb0
Deep_Kapadia <kapadia.deep12@gmail.com>
2019-01-17 18:30:01 +0530

    ScrollView_Swift
    ScrollView

--- ScrollViewSwift/ViewController.swift
+++ ScrollViewSwift/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrl: UIScrollView!
+    
+    @IBOutlet weak var imgv: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imgv.image=UIImage(named: "info.jpg");
+        imgv.contentMode=UIViewContentMode.scaleAspectFit;
+        
+        scrl.contentSize=CGSize(width: imgv.frame.size.width, height: imgv.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
